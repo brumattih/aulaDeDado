ffe9aa9199e75225a0809d005e9ddc15931077f6
Henrique Brumatti <hrpreis@gmail.com>
2021-07-13 09:04:14 -0300

    refatora metodo RolarDado e adiciona metodo Menu

--- Program.cs
+++ Program.cs
@@ -6,27 +6,52 @@ namespace aulaDeDado
     {
         static void Main(string[] args)
         {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1 - Rolar Dado");
+            Console.WriteLine("2 - Sair");
+
+            Console.WriteLine("");
+            Console.WriteLine("Selecione uma opção: ");
+            short resposta = short.Parse(Console.ReadLine());
+
+            switch (resposta)
+            {
+                case 1: RolarDado(); break;
+                case 2: System.Environment.Exit(0); break;
+                default: Console.WriteLine("Opção inválida, Pressione Enter para continuar"); Console.ReadLine(); Menu(); break;
+            }
+
+        }
+
+        static void RolarDado()
+        {
+            Console.Clear();
             Escrever("Digite o número de lados do seu dado: ");
             int quantidadeDeLadosDoDado = LerVariavelEConverterParaInteiro();
 
-            int numeroRolado = RolarDado(quantidadeDeLadosDoDado);
-            Escrever($"Número do dado rolado é: {numeroRolado}");
-        }
-
-        private static int LerVariavelEConverterParaInteiro()
-        {
-            return Convert.ToInt32(Console.ReadLine());
-        }
-
-        static int RolarDado(int quantidadeDeLadosDoDado)
-        {
             Random rd = new Random();
-            return rd.Next(1, quantidadeDeLadosDoDado + 1);
+            int numeroRolado = rd.Next(1, quantidadeDeLadosDoDado + 1);
+
+            Escrever($"Número do dado rolado é: {numeroRolado}");
+            Console.ReadKey();
+            Menu();
         }
 
         static void Escrever(string texto)
         {
             Console.WriteLine(texto);
+        }
+
+        private static int LerVariavelEConverterParaInteiro()
+        {
+            return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
