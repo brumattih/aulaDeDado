0af02d26bced245e1dd39bda43067e568cb173db
Henrique Brumatti <hrpreis@gmail.com>
2021-07-13 08:42:10 -0300

    altera ConvertToInt por metodo LerVariavelEConverterParaInteiro

--- Program.cs
+++ Program.cs
@@ -7,17 +7,21 @@ namespace aulaDeDado
         static void Main(string[] args)
         {
             Escrever("Digite o número de lados do seu dado: ");
-            int quantidadeDeLadosDoDado = Convert.ToInt32(Console.ReadLine());
+            int quantidadeDeLadosDoDado = LerVariavelEConverterParaInteiro();
 
             int numeroRolado = RolarDado(quantidadeDeLadosDoDado);
             Escrever($"Número do dado rolado é: {numeroRolado}");
         }
 
+        private static int LerVariavelEConverterParaInteiro()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
         static int RolarDado(int quantidadeDeLadosDoDado)
         {
             Random rd = new Random();
-            int numeroRolado = rd.Next(1, quantidadeDeLadosDoDado + 1);
-            return numeroRolado;
+            return rd.Next(1, quantidadeDeLadosDoDado + 1);
         }
 
         static void Escrever(string texto)
